03ffc2bbf9af4b08dcaad3fffc6432de1324c5b5
inlym <inlym@qq.com>
2024-03-04 22:35:48 +0800

    :sparkles: 完成公共方法

--- src/core/http.ts
+++ src/core/http.ts
@@ -1,7 +1,8 @@
 import {createAlova} from 'alova'
 import AdapterTaroVue from '@alova/adapter-taro/vue'
+import {environments} from './env'
 
-export const alovaInstant = createAlova({
-  baseURL: 'https://api.lifehelper.com.cn',
+export const alovaInstance = createAlova({
+  baseURL: environments.baseURL,
   ...AdapterTaroVue()
 })
